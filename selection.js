5e26e8b8dd9fc50fb4505923d3f4a36904256938
Gloria Hanzel <gloria.hanzel@gmail.com>
2023-07-21 09:34:37 +0100

    fix player drop issue, add diff colours for teams

--- selection.js
+++ selection.js
@@ -2,15 +2,31 @@
 
 function drag(e) {
   e.dataTransfer.setData("text", e.target.id);
-  e.target.parentElement.classList.remove("selected");
 }
 
 function allowDrop(e) {
   e.preventDefault();
 }
+
 function drop(e) {
-  e.preventDefault();
   let data = e.dataTransfer.getData("text");
-  e.target.appendChild(document.getElementById(data));
-  e.target.classList.add("selected");
+  if (
+    e.target.classList.contains("empty") && // to only allow to create divs in team-position boxes rather than sub-divs of players
+    e.target.children.length == 0 // to only allow one player in each box
+  ) {
+    document
+      .getElementById(data)
+      .parentElement.classList.remove("selected-green");
+    document
+      .getElementById(data)
+      .parentElement.classList.remove("selected-red");
+
+    e.preventDefault();
+    e.target.appendChild(document.getElementById(data));
+    if (e.target.classList.contains("red"))
+      e.target.classList.add("selected-red");
+    else if (e.target.classList.contains("green")) {
+      e.target.classList.add("selected-green");
+    }
+  }
 }
